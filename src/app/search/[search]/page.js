6b1a2202243831fd9c4126491f0501dbe5d80f17
Jeswin A.H <jeswinah@example.com>
2025-05-15 22:31:16 +0530

    search-card updated

--- src/app/search/[search]/page.js
+++ src/app/search/[search]/page.js
@@ -8,6 +8,7 @@ import { useParams } from "next/navigation";
 const Page = () => {
   const [movies, setMovies] = useState([]);
   const { search } = useParams();
+    const [loading, setLoading] = useState(true);
 
   async function apihandler() {
     try {
@@ -15,6 +16,9 @@ const Page = () => {
       setMovies(response.data.results);
     } catch (error) {
       console.error("Error fetching movies:", error);
+    }
+    finally{
+      setLoading(false)
     }
   }
 
@@ -24,10 +28,20 @@ const Page = () => {
     }
   }, [search]);
 
+
+ const placeholderData = Array.from({ length: 20 }, (_, i) => ({
+    id: `placeholder-${i}`,
+    title: "Loading...",
+    backdrop_path: null,
+    vote_average: "-",
+  }));
+
+  const displayData = loading ? placeholderData : movies;
+
   return (
     <div className="grid place-content-center">
       <div className="cards grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 mx-4 sm:gap-8 sm:mx-8">
-        {movies.map(({ id, title, backdrop_path, vote_average }) => {
+        {displayData.map(({ id, title, backdrop_path, vote_average }) => {
           const imgUrl = backdrop_path
             ? `https://image.tmdb.org/t/p/w500/${backdrop_path}`
             : "https://placehold.co/300x168?text=Loading...";
@@ -39,5 +53,4 @@ const Page = () => {
     </div>
   );
 };
-
 export default Page;
